80def775e4dd7ac1933fb5b4e0d505a52aba05b3
DARK\Dark <darkgrouptw@gmail.com>
2018-04-21 21:14:29 +0800

    更新圖片 & Scene 檔

---
+++ Assets/Scripts/UpdateCurrentDate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCurrentDate : MonoBehaviour {
+    // 顯示現在時間的文字
+    public Text CurrentDateText;
+
+	private void Start ()
+    {
+        string FormatedTime = System.DateTime.Now.ToString("yyyy.MM.dd");
+        //Debug.Log(FormatedTime);
+        if (CurrentDateText != null)
+            CurrentDateText.text = FormatedTime;
+        else
+            Debug.Log("沒有文字可以改時間!!");
+	}
+
+}
