5f5ef790c1f7e69ebd507125dde5e5f5f67e66fc
Dark <darkgrouptw@gmail.com>
2018-04-25 15:36:29 +0800

    1. 增加圖片的 Button & Image & Manager 2. Switch 的事件

---
+++ Assets/Scripts/Add Message/SwitchEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchEvent : MonoBehaviour
+{
+    public GameObject TextCanvas;
+    public GameObject VoiceCanvas;
+    public GameObject PhotoCanvas;
+	
+    /// <summary>
+    /// 轉道 Canvas
+    /// </summary>
+    public void SwitchToVoiceCanvas()
+    {
+        SwitchCanvasByBoolean(false, true, false);
+    }
+
+    public void SwtichToPhotCanvas()
+    {
+        SwitchCanvasByBoolean(false, false, true);
+    }
+
+    private void SwitchCanvasByBoolean(bool IsText = false, bool IsVoice = false, bool IsPhoto = false)
+    {
+        TextCanvas.SetActive(IsText);
+        VoiceCanvas.SetActive(IsVoice);
+        PhotoCanvas.SetActive(IsPhoto);
+    }
+}
---
+++ Assets/Scripts/ButtonManager/RecordButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordButtonManager : MonoBehaviour
+{
+    // 圖片
+    [Header("========== 圖片 ==========")]
+    public Sprite DefaultImg;
+    public Sprite RecordBG;
+    public Sprite DisableImg;
+
+    // 物件
+    [Header("========= 物件 =========")]
+    public Image RecordButton;
+
+    #region 按鍵圖片切換 Function
+    private void SwitchToDefault()
+    {
+        RecordButton.sprite = DefaultImg;
+    }
+    private void SwitchToRecording()
+    {
+        RecordButton.sprite = RecordBG;
+    }
+    public void SwitchToDisable()
+    {
+        RecordButton.sprite = DisableImg;
+    }
+    #endregion
+}
