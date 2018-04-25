52690183a7446569017cbd2074e6e6e7e21f1674
DARK\Dark <darkgrouptw@gmail.com>
2018-04-25 18:59:31 +0800

    1. 更新 Canvas 切換 & AR Camera 2. 增加 Canvas 切換的 Script

---
+++ Assets/Scripts/CanvasEvent/CanvasChangeEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasChangeEvent : MonoBehaviour
+{
+    [Header("========== Canvas ==========")]
+    public GameObject MainCanvas;
+    public GameObject AddMessageCanvas;
+
+    public GameObject ARCamera;
+
+    public void MainToAddMessage()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+
+        MainCanvas.SetActive(false);
+        AddMessageCanvas.SetActive(true);
+
+        ARCamera.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeRight;
+
+        MainCanvas.SetActive(true);
+        AddMessageCanvas.SetActive(false);
+
+        ARCamera.SetActive(true);
+    }
+}
